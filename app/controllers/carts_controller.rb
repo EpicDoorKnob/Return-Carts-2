9c37fabb8fdb5a53d87bd4d6b3d1e9bf8481bde0
Mark Rosenberg <Mark8wr@aol.com>
2018-01-25 00:41:14 -0500

    I can make carts now, but I want the user_id to happen based off current_user

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,38 @@
 class CartsController < ApplicationController
 
+
     def index
         @carts = Cart.all
     end
+
+    def show
+    end
+
+    def new
+        @cart = Cart.new
+    end
+
+       def create
+         @cart = Cart.new(cart_params)
+        if @cart.save
+            flash[:success] = "Let's load this cart!"
+            redirect_to carts_cart_path
+        else
+            flash[:failure] = "Shit didn't work :("
+            redirect_to carts_cart_path
+        end
+    end
+
+     private
+
+     def boop
+         @user = User.first.email
+    end
+
+    def cart_params
+        params.permit(:user_id, current_user, :muid)
+    end
+
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'static_pages#home'
-  get   'products/new'
+  get   '/products/new'
   get   '/help',                to: 'static_pages#help'
-  get   '/addsku',              to: 'products#new'
-  get   '/index',               to: 'products#index'
+  get   '/products/addsku',     to: 'products#new'
+  get   '/products/index',      to: 'products#index'
   get   '/carts',               to: 'carts#index'
+  get   '/carts/cart',          to: 'carts#cart'
+  post  '/carts/new',           to: 'carts#create'
 
   resources :products
+  resources :carts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
