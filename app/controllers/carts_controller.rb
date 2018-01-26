2c56c7d01b134b1aa8a3816ccc3c005d29c7ba65
Mark Rosenberg <Mark8wr@aol.com>
2018-01-25 19:06:59 -0500

    I can add carts now and the user Id is automatic

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,7 +13,7 @@ class CartsController < ApplicationController
     end
 
        def create
-         @cart = Cart.new(cart_params)
+        @cart = current_user.carts.build(cart_params)
         if @cart.save
             flash[:success] = "Let's load this cart!"
             redirect_to carts_cart_path
@@ -30,7 +30,7 @@ class CartsController < ApplicationController
     end
 
     def cart_params
-        params.permit(:user_id, current_user, :muid)
+        params.permit(:current_user, :muid)
     end
 
 
