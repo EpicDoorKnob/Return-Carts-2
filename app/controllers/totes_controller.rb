23c031cb51042da2e5f86c1dfb21ed5345d5ee8e
Mark Rosenberg <Mark8wr@aol.com>
2018-02-16 01:39:36 -0500

    Position logic and cart size work  guddddddddd

--- app/controllers/totes_controller.rb
+++ app/controllers/totes_controller.rb
@@ -1,29 +1,41 @@
 class TotesController < ApplicationController
 require 'json'
-  def index
-    @totes = Tote.all 
-  end
 
-    def create
-        @current_totes = Tote.where(cart_id: @current_cart_id)
-        @current_cart_id = current_user.carts.where(active: "true").first.id
-        @upc = Product.find_by(upc: params[:product_upc])
-        @tote = Tote.new(tote_params)
-        @tote.product_id = @upc.id
-        @tote.cart_id = current_user.carts.where(active: "true").first.id
-        @tote.qty = Tote.where(cart_id: @current_cart_id).pluck(:product_id).count(@tote.product_id) + 1
+def index
+@totes = Tote.all 
+end
+
+def create
+  @current_totes = Tote.where(cart_id: @current_cart_id)
+  @current_cart_id = current_user.carts.where(active: "true").first.id
+  @upc = Product.find_by(upc: params[:product_upc])
+  @tote = Tote.new(tote_params)
+  @tote.product_id = @upc.id
+  if @upc.id.present?
+    @tote.cart_id = current_user.carts.where(active: "true").first.id
+    @tote.qty = Tote.where(cart_id: @current_cart_id).pluck(:product_id).count(@tote.product_id) + 1
+      if Tote.where(cart_id: @current_cart_id).pluck(:product_id).include?(@upc.id)
+        @tote.position = Tote.where(cart_id: @current_cart_id).where(product_id: @upc.id).first.position
+      else
         if Tote.where(cart_id: @current_cart_id).blank?
-            @tote.position = 1
+          @tote.position = 1
         else
-            
+          @tote.position = Tote.where(cart_id: @current_cart_id).pluck(:position).max.to_i + 1
+        end
+      end
+        if @tote.position.to_i > current_user.carts.where(active: "true").first.size
+          flash[:danger]="You cannot add any more product to this cart!"
+          redirect_to new_tote_path and return
         end
         if @tote.save
-            flash[:success]="#{current_user.carts.where(active: "true").first.totes.last.product.sku} goes in position
-                                        #{current_user.carts.where(active: "true").first.totes.last.position}"
-            redirect_to new_tote_path
+          flash[:success]="Position #{current_user.carts.where(active: "true").first.totes.last.position}"
+          redirect_to new_tote_path
         end
+        else
+      end
+    else
     end
-
+  
     def new
         @tote = Tote.new
         @current_cart_id = current_user.carts.where(active: "true").first.id
@@ -39,7 +51,7 @@ require 'json'
     private
 
     def tote_params
-        params.require(:tote).permit(:position, :qty)
+        params.permit
     end
 
 end
