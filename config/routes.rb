613e0c9064fd540bdf654d6d148119560c9105c8
Mark Rosenberg <Mark8wr@aol.com>
2018-02-22 00:07:52 -0500

    UI changes with kendall

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  'static_pages#home'
   get   '/products/new'
   get   '/help',                to: 'static_pages#help'
   get   '/products/addsku',     to: 'products#new'
@@ -9,10 +8,12 @@ Rails.application.routes.draw do
   get   '/carts/cart',          to: 'carts#cart'
   post  '/carts/new',           to: 'carts#create'
   post  '/totes/new',           to: 'totes#create'
+  post  '/totes/new',           to: 'totes#new'
   post  '/carts/edit',          to: 'carts#edit'
   resources :products
   resources :carts
   resources :totes
+  root  'carts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
